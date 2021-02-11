26f866300d282bee560dd2cd2d239fc60c4ddcff
khobar <khobar@pop-os.localdomain>
2021-02-11 16:39:15 +0700

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd763918b08c2abf4fcf2489502669eda'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '322e93dd-09f2-4fd7-a531-306a09f6d404'
+};
